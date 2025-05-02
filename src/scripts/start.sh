7da874d0a43ab2cb1e75a487e481607609803830
Micah Worth <mjw324@lehigh.edu>
2025-05-01 22:39:54 -0400

    updated start script to match internal db port

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -2,7 +2,7 @@
 
 # Wait for database to be ready
 echo "Waiting for database to be ready..."
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+while ! nc -z $POSTGRES_HOST 5432; do
   sleep 1
 done
 echo "Database is ready!"
