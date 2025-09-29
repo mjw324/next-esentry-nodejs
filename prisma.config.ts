d91c3177fd0e97080d1fb4d668bb98296cfe3d19
Micah Worth <mjw324@lehigh.edu>
2025-09-29 11:48:27 -0400

    Fixed schema file path

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "prisma/config";
-import path from "path";
 
 export default defineConfig({
-  schema: path.join("node_modules", "@mjw324", "prisma-shared", "prisma", "schema.prisma"),
+  schema: "node_modules/@mjw324/prisma-shared/prisma/schema.prisma",
 });
